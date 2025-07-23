a89e742989329a87584a001408fced6c5ccdedfa
AhmedKhaled858 <tetaahmed858@gmail.com>
2025-07-23 11:30:46 +0300

    make provide for add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/core/utiles/app_router.dart';
+import 'package:note_app/features/notes/presentation/manger/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
 import 'core/utiles/constant.dart';
@@ -17,19 +19,24 @@ class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-       builder: (context, child) => ResponsiveBreakpoints.builder(
-        child: child!,
-        breakpoints: [
-          const Breakpoint(start: 0, end: 450, name: MOBILE),
-          const Breakpoint(start: 451, end: 800, name: TABLET),
-          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
-          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
-        ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp.router(
+         builder: (context, child) => ResponsiveBreakpoints.builder(
+          child: child!,
+          breakpoints: [
+            const Breakpoint(start: 0, end: 450, name: MOBILE),
+            const Breakpoint(start: 451, end: 800, name: TABLET),
+            const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+            const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+          ],
+        ),
+        theme: ThemeData.dark(),
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
       ),
-      theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
     );
   }
 }
