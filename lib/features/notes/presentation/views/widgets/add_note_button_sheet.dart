a52c19b79ef41f2247863df3c865b413910844b3
AhmedKhaled858 <tetaahmed858@gmail.com>
2025-07-24 13:24:21 +0300

    create simple bloc observer

--- lib/features/notes/presentation/views/widgets/add_note_button_sheet.dart
+++ lib/features/notes/presentation/views/widgets/add_note_button_sheet.dart
@@ -1,7 +1,12 @@
 // ignore_for_file: must_be_immutable, use_build_context_synchronously, depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
+import '../../manger/cubits/add_note_cubit/add_note_cubit.dart';
 import 'add_note_form.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
@@ -9,7 +14,7 @@ class AddNoteButtonSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return Padding(
+    return Padding(
       padding: EdgeInsets.only(
         left: 16,
         right: 16,
@@ -18,7 +23,32 @@ class AddNoteButtonSheet extends StatelessWidget {
       ),
       child: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
-        child:  AddNoteForm(),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+                  showTopSnackBar(
+                      Overlay.of(context),
+                      const CustomSnackBar.success(
+                        message: "Note created successfully!",
+                      ),
+                    );
+            } else if (state is AddNoteFailure) {
+              showTopSnackBar(
+                Overlay.of(context),
+                CustomSnackBar.error(
+                  message: 'Failed to add note: ${state.errorMessage}',
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: AddNoteForm(),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/features/notes/presentation/views/widgets/add_note_form.dart
+++ lib/features/notes/presentation/views/widgets/add_note_form.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: depend_on_referenced_packages, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
@@ -11,62 +10,61 @@ class AddNoteForm extends StatefulWidget {
   const AddNoteForm({super.key});
   @override
   State<AddNoteForm> createState() => _AddNoteFormState();
-
 }
 
 class _AddNoteFormState extends State<AddNoteForm> {
-   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
- AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
 
-  String?title,subtitle;
+  String? title, subtitle;
   @override
   Widget build(BuildContext context) {
     return Form(
       key: formKey,
       autovalidateMode: autovalidateMode,
       child: Column(
-            children: [
-              CustomTextField(
-                onSaved: (value){
-                  title = value;
-                },
-                labelText: "title",
-                hintText: "write your title here",
-              ),
-             const SizedBox(height: 25),
-              CustomTextField(
-                maxLines: 5,
-                labelText: "content",
-                hintText: "write your note here",
-                onSaved: (value){
-                  subtitle = value;
-                },
-              ),
-             const SizedBox(height: 60),
-            CustomAddButton(
-                onTap: () {
-                  if (formKey.currentState!.validate()) {
-                    formKey.currentState!.save();
-                    // ✅ Show top success message
-                    showTopSnackBar(
-                      Overlay.of(context),
-                      const CustomSnackBar.success(
-                        message: "Note created successfully!",
-                      ),
-                    );
-
-                    Future.delayed(const Duration(milliseconds: 500), () {
-                      Navigator.pop(context);
-                    });
-                  } else {
-                    setState(() {
-                      autovalidateMode = AutovalidateMode.always;
-                    });
-                  }
-                },
-              ),
-            ],
+        children: [
+          CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
+            labelText: "title",
+            hintText: "write your title here",
           ),
+          const SizedBox(height: 25),
+          CustomTextField(
+            maxLines: 5,
+            labelText: "content",
+            hintText: "write your note here",
+            onSaved: (value) {
+              subtitle = value;
+            },
+          ),
+          const SizedBox(height: 60),
+          CustomAddButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+                // ✅ Show top success message
+                showTopSnackBar(
+                  Overlay.of(context),
+                  const CustomSnackBar.success(
+                    message: "Note created successfully!",
+                  ),
+                );
+
+                Future.delayed(const Duration(milliseconds: 500), () {
+                  Navigator.pop(context);
+                });
+              } else {
+                setState(() {
+                  autovalidateMode = AutovalidateMode.always;
+                });
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
