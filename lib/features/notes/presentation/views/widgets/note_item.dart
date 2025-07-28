22df199cbde0f00b96b48a660b2a2190b3360dea
AhmedKhaled858 <tetaahmed858@gmail.com>
2025-07-28 13:39:44 +0300

    create delete icon

--- lib/features/notes/presentation/views/widgets/note_item.dart
+++ lib/features/notes/presentation/views/widgets/note_item.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:note_app/core/utiles/app_router.dart';
 import 'package:note_app/features/notes/data/models/note_model.dart';
+import 'package:note_app/features/notes/presentation/views/widgets/delete_icon.dart';
 import 'package:top_snackbar_flutter/custom_snack_bar.dart';
 import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
@@ -17,7 +18,7 @@ class NoteItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        GoRouter.of(context).go(AppRouter.KEditNoteView,  extra: note,);
+        GoRouter.of(context).go(AppRouter.KEditNoteView, extra: note);
       },
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 4),
@@ -72,57 +73,7 @@ class NoteItem extends StatelessWidget {
                     SizedBox(
                       height: 40,
                       width: 40,
-                      child: IconButton(
-                        padding: EdgeInsets.zero,
-                        icon: Icon(Icons.delete, color: Colors.white, size: 24),
-                        onPressed: () {
-                          //من الاخر مينفعش ابني context داخل الديالوج ف خدته في فاريبال وناديته 
-                          
-                          final parentContext = context; // Save parent context
-
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              title: const Text("Delete Note"),
-                              content: const Text(
-                                "Are you sure you want to delete this note?",
-                              ),
-                              actions: [
-                                TextButton(
-                                  onPressed: () => Navigator.of(context).pop(),
-                                  child: const Text("Cancel"),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    note.delete();
-
-                                    // ✅ Use parentContext here, NOT dialog context
-                                    BlocProvider.of<NotesCubit>(
-                                      parentContext,
-                                    ).fetchNotes();
-
-                                    Navigator.of(context).pop(); // Close dialog
-
-                                    showTopSnackBar(
-                                      Overlay.of(
-                                        parentContext,
-                                      ), // Use parent context here too
-                                      const CustomSnackBar.success(
-                                        message: "Note deleted successfully!",
-                                        backgroundColor: Color.fromARGB(255, 13, 243, 16),
-                                      ),
-                                    );
-                                  },
-                                  child: const Text(
-                                    "Delete",
-                                    style: TextStyle(color: Colors.red),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                      ),
+                      child: DeleteIcon(note:note)
                     ),
                   ],
                 ),
